33cd1d8c2b479cae9aa13a8f2ab56d42536351cf
helloworld21951 <niknaglov99@gmail.com>
2023-11-09 23:18:43 +0300

    Initial commit on PS1

---
+++ PS1/nikolay_naglov_ps1/rod_solve.py
@@ -0,0 +1,13 @@
+import numpy as np
+import os
+
+from utils import *
+
+filename = "data_ps1.npz"
+
+if not os.path.isfile(filename):
+    raise Exception("The data file {} does not exist".format(filename))
+
+with np.load(filename) as data:
+    print(f"Environment:\n{data['environment']}")
+    print(f"Rod:\n{data['rod']}")
