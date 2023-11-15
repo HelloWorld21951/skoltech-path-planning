0e1d11b9cc5810010548abe92d0ade5946eb0edc
HelloWorld21951 <niknaglov99@gmail.com>
2023-11-15 12:27:15 +0300

    Task 1C

--- PS1/ivan_berendiaev_ps1/main.py
+++ PS1/ivan_berendiaev_ps1/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import convolve2d
 
 from utils import *
  
@@ -20,6 +21,17 @@ def main():
     state = (10, 10, 3)
     rod_in_environment = plot_enviroment(environment, rod, state)
     plt.imsave(f"images/rod_in_environment.png", rod_in_environment)
+
+    # Task 1C
+
+    cspace = np.empty(
+        (environment.shape[0], environment.shape[1], rod.shape[2])
+    )
+    for i in range(rod.shape[2]):
+        cspace[:, :, i] = normalize_image(
+            convolve2d(environment, rod[:, :, i], "same", "symm")
+        )
+        plt.imsave(f"images/cspace_{i}.png", cspace[:, :, i])
 
 if __name__ == "__main__":
     main()
