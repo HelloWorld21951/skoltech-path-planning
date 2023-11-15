8288e5316dc1f5729aa9448e3a6ef775134e48b9
HelloWorld21951 <niknaglov99@gmail.com>
2023-11-15 12:16:01 +0300

    Task 1B

--- PS1/ivan_berendiaev_ps1/main.py
+++ PS1/ivan_berendiaev_ps1/main.py
@@ -1,17 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+from utils import *
  
 def main():
     data = np.load("data_ps1.npz")
     environment = data["environment"]
     rod = data["rod"]
 
+    # Task 1A
+
     for i in range(0, rod.shape[2]):
-        plt.imsave(
-            f"images/rod_config_{i}.png", rod[:, :, i]
-        )
+        plt.imsave(f"images/rod_config_{i}.png", rod[:, :, i])
 
     plt.imsave(f"images/environment.png", environment)
+
+    # Task 1B
+
+    state = (10, 10, 3)
+    rod_in_environment = plot_enviroment(environment, rod, state)
+    plt.imsave(f"images/rod_in_environment.png", rod_in_environment)
 
 if __name__ == "__main__":
     main()
