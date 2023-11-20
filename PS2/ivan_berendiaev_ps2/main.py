2f223c6eaf75f03778fbd72e32a863eb1c42743a
helloworld21951 <niknaglov99@gmail.com>
2023-11-20 19:12:19 +0300

    Task 1B

--- PS2/ivan_berendiaev_ps2/main.py
+++ PS2/ivan_berendiaev_ps2/main.py
@@ -23,10 +23,40 @@ def main():
 
     # Task 1A
 
-    env.render(plt_imsave=True, save_path="images/start_state.png")
+    # env.render(plt_imsave=True, save_path="images/start_state.png")
 
-    env.state = goal_state
-    env.render(plt_imsave=True, save_path="images/goal_state.png")
+    # env.state = goal_state
+    # env.render(plt_imsave=True, save_path="images/goal_state.png")
+
+    # Task 1B
+
+    random_state_1 = State(np.array([90, 0, 90, 0]))
+    env.state = random_state_1
+    print(
+        f"Random state: [{random_state_1.angles}]\tCollision: {env.check_collision(random_state_1)}"
+    )
+    env.render(plt_imsave=True, save_path="images/random_state_1.png")
+
+    random_state_2 = State(np.array([0, 90, -45, -45]))
+    env.state = random_state_2
+    print(
+        f"Random state: [{random_state_2.angles}]\tCollision: {env.check_collision(random_state_2)}"
+    )
+    env.render(plt_imsave=True, save_path="images/random_state_2.png")
+
+    random_state_3 = State(np.array([-135, -135, 90, 0]))
+    env.state = random_state_3
+    print(
+        f"Random state: [{random_state_3.angles}]\tCollision: {env.check_collision(random_state_3)}"
+    )
+    env.render(plt_imsave=True, save_path="images/random_state_3.png")
+
+    random_state_4 = State(np.array([-90, 90, 0, 0]))
+    env.state = random_state_4
+    print(
+        f"Random state: [{random_state_4.angles}]\tCollision: {env.check_collision(random_state_4)}"
+    )
+    env.render(plt_imsave=True, save_path="images/random_state_4.png")
 
     # planner = RRTPlanner(env)
 
