e48280089eb6092a105d50a0a65ed2810b51cb94
HelloWorld21951 <niknaglov99@gmail.com>
2023-12-09 22:47:48 +0300

    Task 1

--- PS3/nikolay_naglov_ps3/run.py
+++ PS3/nikolay_naglov_ps3/run.py
@@ -7,37 +7,40 @@ from vi import vi, policy_vi
 from mdp import mdp, policy_mdp
 
 
-data = np.load('data_ps3.npz')
-environment = data['environment']
-#plt.matshow(environment)
-
+data = np.load("data_ps3.npz")
+environment = data["environment"]
 
 # (row index, colum index). In the image row corresponds to y, and colum to x.
-x_ini = (11,6)
-goal = (15,29)
-
-# Visualization
-# ======================================
-if 1:
-    im = plot_enviroment(environment,x_ini,goal)
-    plt.matshow(im)
-    plt.show()
+x_ini = (11, 6)
+goal = (15, 29)
 
 
 # task 1 VI, Gopt
 # ======================================
-Gopt = vi(environment,goal)
+task_1_folder = "results/task_1"
+Gopt = vi(environment, goal)
+plt.imshow(Gopt)
+plt.savefig(f"{task_1_folder}/final_G.png")
+plt.clf()
 
+iterations = [1]
+iterations.extend(list(range(10, 70, 10)))
+
+for i in iterations:
+    G = vi(environment, goal, max_num_of_iterations=i)
+    plt.imshow(G)
+    plt.savefig(f"{task_1_folder}/G_{i}_iters.png")
+    plt.clf()
 
 
 # task 2: Plan
 # ======================================
-policy = policy_vi(environment,Gopt)
+policy = policy_vi(environment, Gopt)
 
 
 # task 3 MDP, Vopt
 # ======================================
-#Vopt = mdp(environment,goal)
+# Vopt = mdp(environment,goal)
 
 # Visualization
 # ======================================
@@ -46,22 +49,20 @@ if 0:
     imgs = []
     x = x_ini
     for plan_iters in range(100):
-        im = plot_enviroment(environment,x,goal)
+        im = plot_enviroment(environment, x, goal)
         plot = plt.imshow(im)
         imgs.append([plot])
-        
+
         # TODO, calculate a plan based on the policy calcualted in VI or MDP
-        
-        
+
         if x == goal:
-            print('Goal achieved in iters =', plan_iters)
+            print("Goal achieved in iters =", plan_iters)
             break
 
-
-    im = plot_enviroment(environment,x,goal)
+    im = plot_enviroment(environment, x, goal)
     plot = plt.imshow(im)
     imgs.append([plot])
     ani = animation.ArtistAnimation(fig, imgs, interval=100, blit=True)
-    ani.save('plan_vi.mp4')
-    #ani.save('plan_mdp.mp4')
+    ani.save("plan_vi.mp4")
+    # ani.save('plan_mdp.mp4')
     plt.show()
--- PS3/nikolay_naglov_ps3/vi.py
+++ PS3/nikolay_naglov_ps3/vi.py
@@ -1,25 +1,46 @@
 import numpy as np
 from utils import action_space, transition_function
 
-def vi(env, goal):
+
+def vi(env, goal, max_num_of_iterations=100):
     """
     env is the grid enviroment
     goal is the goal state
-    
+
     output:
     G: Optimal cost-to-go
     """
-    
+
+    G = np.full(env.shape, np.inf)
+    G[goal] = 0
+    for i in range(max_num_of_iterations):
+        new_G = G.copy()
+        for row in range(G.shape[0]):
+            for column in range(G.shape[1]):
+                current_state = (row, column)
+                for action in action_space:
+                    transition_state, success = transition_function(
+                        env, current_state, action
+                    )
+                    if not success:
+                        continue
+                    new_G[transition_state] = min(
+                        new_G[transition_state], G[current_state] + 1
+                    )
+        if np.array_equal(G, new_G):
+            print(f"Converged after {i} iterations")
+            break
+        G = new_G
     return G
 
 
-def policy_vi(env,G):
+def policy_vi(env, G):
     """
     env is the grid enviroment
-    G: optimal cot-to-go function
-    
+    G: optimal cost-to-go function
+
     output:
     policy: a map from each state x to the best action a to execcute
     """
-    
+    policy = {}
     return policy
